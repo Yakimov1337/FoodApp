89dc6ef343594a5c7e7a96049b19808f6312cae8
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-14 03:09:44 +0200

    imageUrl type changed

--- src/app/core/models/food-menu-item.model.ts
+++ src/app/core/models/food-menu-item.model.ts
@@ -3,7 +3,7 @@ export interface MenuItem {
   title: string; // Title of the menu item.
   description: string; // Description of the menu item.
   price: number; // Price of the menu item.
-  imageUrl: string; // URL to the image of the menu item.
+  imageUrl: URL; // URL to the image of the menu item.
   salesCount: number; // Number of times this item has been ordered.
   default: boolean; //Determinate if item is created by admin or normal user
 }
