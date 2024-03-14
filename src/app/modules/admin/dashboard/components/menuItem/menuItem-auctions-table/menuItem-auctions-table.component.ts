01d6358101cc3b0c177f919d8526342b2698cc8d
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-14 03:11:41 +0200

    impl: Loader

--- src/app/modules/admin/dashboard/components/menuItem/menuItem-auctions-table/menuItem-auctions-table.component.ts
+++ src/app/modules/admin/dashboard/components/menuItem/menuItem-auctions-table/menuItem-auctions-table.component.ts
@@ -1,27 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuItem } from '../../../models/food-menu-item';
+import { Component, Input, OnInit } from '@angular/core';
 import { MenuItemAuctionsTableItemComponent } from '../menuItem-auctions-table-item/menuItem-auctions-table-item.component';
-import { NgFor } from '@angular/common';
-import { OrdersService } from '../../../../../../services/orders.service';
+import { CommonModule, NgFor } from '@angular/common';
+import { MenuItemsService } from '../../../../../../services/menuItems.service';
+import { MenuItem } from '../../../../../../core/models';
+import { LoaderComponent } from '../../../../../../shared/components/loader/loader.component';
 
 @Component({
   selector: '[menuItem-auctions-table]',
   templateUrl: './menuItem-auctions-table.component.html',
   standalone: true,
-  imports: [NgFor, MenuItemAuctionsTableItemComponent],
+  imports: [NgFor, MenuItemAuctionsTableItemComponent, CommonModule, LoaderComponent],
 })
 export class MenuItemAuctionsTableComponent implements OnInit {
-  public activeAuction: MenuItem[] = [];
+  @Input() items: MenuItem[] = [];
+  @Input() isLoading: boolean = false;
 
-  constructor(private ordersService: OrdersService) {} 
+  constructor() {}
 
-  async ngOnInit(): Promise<void> {
-    try {
-      // Call getAllOrders() from the orders service
-      this.activeAuction = await this.ordersService.getAllOrders();
-    } catch (error) {
-      console.error('Error fetching orders:', error);
-      // Handle error appropriately
-    }
-  }
+  async ngOnInit(): Promise<void> {}
 }
