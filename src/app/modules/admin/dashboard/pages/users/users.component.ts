e77136747207bc15f5cdae6de36291f46874bd9d
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-17 21:30:36 +0200

    impl: delete user modal

--- src/app/modules/admin/dashboard/pages/users/users.component.ts
+++ src/app/modules/admin/dashboard/pages/users/users.component.ts
@@ -1,29 +1,15 @@
 import { Component } from '@angular/core';
-
-import { User } from '../../../../../core/models';
-import { UserService } from '../../../../../services/user.service';
 import { UsersTableComponent } from '../../components/users/users-table/users-table.component';
+import { UserCreateModalComponent } from '../../components/users/user-create-modal/user-create-modal.component';
+import { UserDeleteModalComponent } from '../../components/users/user-delete-modal/user-delete-modal.component';
+import { UserUpdateModalComponent } from '../../components/users/user-update-modal/user-update-modal.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [UsersTableComponent],
+  imports: [UsersTableComponent, UserCreateModalComponent, UserDeleteModalComponent, UserUpdateModalComponent],
   templateUrl: './users.component.html',
 })
 export class UsersComponent {
-  public users: User[] = [];
-  public isLoading: boolean = true;
-
-  constructor(private userService: UserService) {}
-
-  async ngOnInit(): Promise<void> {
-    try {
-      // Call getAllUsers() from the Users service
-      this.users = await this.userService.getAllUsers();
-      this.isLoading = false;
-    } catch (error) {
-      console.error('Error fetching Users:', error);
-      this.isLoading = false;
-    }
-  }
+  constructor() {}
 }
