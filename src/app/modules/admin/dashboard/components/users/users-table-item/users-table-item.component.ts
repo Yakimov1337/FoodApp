19d6108e57320d6e834a6a2c8a0d51154ce46a9c
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-17 21:31:54 +0200

    dispatch modals

--- src/app/modules/admin/dashboard/components/users/users-table-item/users-table-item.component.ts
+++ src/app/modules/admin/dashboard/components/users/users-table-item/users-table-item.component.ts
@@ -1,20 +1,28 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule, CurrencyPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
+import { Store } from '@ngrx/store';
 import { User } from '../../../../../../core/models';
 import { ButtonComponent } from '../../../../../../shared/components/button/button.component';
-
-
+import { openDeleteUserModal, openUpdateUserModal } from '../../../../../../core/state/modal/modal.actions';
 @Component({
-    selector: '[users-table-item]',
-    templateUrl: './users-table-item.component.html',
-    standalone: true,
-    imports: [AngularSvgIconModule, CurrencyPipe, ButtonComponent,CommonModule],
+  selector: '[users-table-item]',
+  templateUrl: './users-table-item.component.html',
+  standalone: true,
+  imports: [CommonModule, AngularSvgIconModule, ButtonComponent],
 })
 export class UsersTableItemComponent implements OnInit {
   @Input() user: User = <User>{};
-  constructor() {}
 
+  constructor(private store: Store) {}
 
   ngOnInit(): void {}
+
+  openUpdateModal() {
+    this.store.dispatch(openUpdateUserModal({ user: this.user }));
+  }
+
+  openDeleteModal() {
+    this.store.dispatch(openDeleteUserModal({ userId: this.user.$id }));
+  }
 }
