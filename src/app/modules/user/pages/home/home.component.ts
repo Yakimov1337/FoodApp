6ad53cc2b4711953ce878a16a59daacaa302a3ca
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-17 21:27:20 +0200

    Router module import

--- src/app/modules/user/pages/home/home.component.ts
+++ src/app/modules/user/pages/home/home.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   constructor(private router: Router) {}
-
-  goToAdminDashboard(): void {
-    this.router.navigate(['/admin/dashboard']);
-  }
 
 }
--- src/app/modules/user/user.component.ts
+++ src/app/modules/user/user.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
