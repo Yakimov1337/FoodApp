0ec77370902561fe7b4fec083091cfca226edbd0
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-18 23:15:39 +0200

    Update order.model.ts

--- src/app/core/models/order.model.ts
+++ src/app/core/models/order.model.ts
@@ -1,9 +1,10 @@
 import { MenuItem } from "./food-menu-item.model";
+import { User } from "./user.model";
 
 export interface Order {
-  $id: string; 
-  userId: string;
-  items: MenuItem[];
+  $id: string;
+  user: User;
+  menuItem: MenuItem;
   totalCost: number;
   createdOn: Date;
   status: 'pending' | 'completed' | 'cancelled';
