d3818f29dbfe1cc7d73ff565ec773b6375b708ed
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-21 00:38:54 +0200

    impl: items modals

--- src/app/modules/admin/dashboard/pages/items/items.component.ts
+++ src/app/modules/admin/dashboard/pages/items/items.component.ts
@@ -1,28 +1,28 @@
+import { Store } from '@ngrx/store';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MenuItemAuctionsTableComponent } from '../../components/menuItem/menuItem-auctions-table/menuItem-auctions-table.component';
-import { MenuItem } from '../../../../../core/models';
-import { MenuItemsService } from '../../../../../services/menuItems.service';
+import { MenuItemCreateModalComponent } from '../../components/menuItem/menuItem-create-modal/menuItem-create-modal.component';
+import { MenuItemDeleteModalComponent } from '../../components/menuItem/menuItem-delete-modal/menuItem-delete-modal.component';
+import { MenuItemUpdateModalComponent } from '../../components/menuItem/menuItem-update-modal/menuItem-update-modal.component';
+import { selectIsCreateMenuItemModalOpen, selectIsDeleteMenuItemModalOpen, selectIsUpdateMenuItemModalOpen } from '../../../../../core/state/modal/menuItem/modal.selectors';
 
 @Component({
   selector: 'app-items',
   standalone: true,
-  imports: [MenuItemAuctionsTableComponent],
+  imports: [
+    CommonModule,
+    MenuItemAuctionsTableComponent,
+    MenuItemCreateModalComponent,
+    MenuItemDeleteModalComponent,
+    MenuItemUpdateModalComponent,
+  ],
   templateUrl: './items.component.html',
 })
 export class ItemsComponent {
-  public items: MenuItem[] = [];
-  public isLoading: boolean = true;
+  showCreateMenuItemModal$ = this.store.select(selectIsCreateMenuItemModalOpen);
+  showDeleteMenuItemModal$ = this.store.select(selectIsDeleteMenuItemModalOpen);
+  showUpdateMenuItemModal$ = this.store.select(selectIsUpdateMenuItemModalOpen);
 
-  constructor(private menuItemsService: MenuItemsService) {}
-
-  async ngOnInit(): Promise<void> {
-    try {
-      // Call getAllMenuItems() from the MenuItems service
-      this.items = await this.menuItemsService.getAllMenuItems();
-      this.isLoading = false;
-    } catch (error) {
-      console.error('Error fetching MenuItems:', error);
-      this.isLoading = false;
-    }
-  }
+  constructor(private store: Store) {}
 }
