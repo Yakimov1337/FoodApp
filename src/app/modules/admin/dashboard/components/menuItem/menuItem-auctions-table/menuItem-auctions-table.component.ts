d7af74dfed502609b8026e7adc42693048b3a484
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-20 23:41:42 +0200

    impl: items table

--- src/app/modules/admin/dashboard/components/menuItem/menuItem-auctions-table/menuItem-auctions-table.component.ts
+++ src/app/modules/admin/dashboard/components/menuItem/menuItem-auctions-table/menuItem-auctions-table.component.ts
@@ -1,21 +1,83 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { MenuItemAuctionsTableItemComponent } from '../menuItem-auctions-table-item/menuItem-auctions-table-item.component';
 import { CommonModule, NgFor } from '@angular/common';
 import { MenuItemsService } from '../../../../../../services/menuItems.service';
 import { MenuItem } from '../../../../../../core/models';
 import { LoaderComponent } from '../../../../../../shared/components/loader/loader.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { ButtonComponent } from '../../../../../../shared/components/button/button.component';
+import { openCreateMenuItemModal } from '../../../../../../core/state/modal/menuItem/modal.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: '[menuItem-auctions-table]',
   templateUrl: './menuItem-auctions-table.component.html',
   standalone: true,
-  imports: [NgFor, MenuItemAuctionsTableItemComponent, CommonModule, LoaderComponent],
+  imports: [
+    NgFor,
+    AngularSvgIconModule,
+    ButtonComponent,
+    MenuItemAuctionsTableItemComponent,
+    CommonModule,
+    LoaderComponent,
+  ],
 })
 export class MenuItemAuctionsTableComponent implements OnInit {
-  @Input() items: MenuItem[] = [];
-  @Input() isLoading: boolean = false;
+  public menuItems: MenuItem[] = [];
+  public isLoading: boolean = true;
+  private subscriptions: Subscription = new Subscription();
 
-  constructor() {}
+  constructor(private menuItemsService: MenuItemsService, private store: Store) {}
 
-  async ngOnInit(): Promise<void> {}
+  ngOnInit(): void {
+    //Fetch items
+    const menuItemsSub = this.menuItemsService.getAllMenuItems().subscribe({
+      next: (menuItems) => {
+        this.menuItems = menuItems;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching items:', error);
+        this.isLoading = false;
+      },
+    });
+    const menuItemsCreatedSub = this.menuItemsService.menuItemCreated$.subscribe((menuItem) => {
+      if (menuItem) {
+        this.reloadOrders(); // Refetch items after a new item is created
+      }
+    });
+    const menuItemsDeletedSub = this.menuItemsService.menuItemDeleted$.subscribe((deleteMenuItemId) => {
+      if (deleteMenuItemId) {
+        this.menuItems = this.menuItems.filter((menuItem) => menuItem.$id !== deleteMenuItemId);
+      }
+    });
+    this.subscriptions.add(menuItemsCreatedSub);
+    this.subscriptions.add(menuItemsDeletedSub);
+    this.subscriptions.add(menuItemsSub);
+  }
+
+  reloadOrders(): void {
+    this.isLoading = true;
+    this.menuItemsService.getAllMenuItems().subscribe({
+      next: (menuItems) => {
+        console.log('Fetched items:', menuItems);
+        this.menuItems = menuItems;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Error fetching items:', error);
+        this.isLoading = false;
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
+  openCreateModal() {
+    console.log('Dispatching openCreateOrderModal action');
+    this.store.dispatch(openCreateMenuItemModal());
+  }
 }
