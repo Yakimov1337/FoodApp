376176dd1b3b04017d0fca9d79087fe7f9898270
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-20 23:41:23 +0200

    impl: item modals

--- src/app/modules/admin/dashboard/components/menuItem/menuItem-auctions-table-item/menuItem-auctions-table-item.component.ts
+++ src/app/modules/admin/dashboard/components/menuItem/menuItem-auctions-table-item/menuItem-auctions-table-item.component.ts
@@ -3,17 +3,31 @@ import { CurrencyPipe } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { MenuItem } from '../../../../../../core/models';
 import { ButtonComponent } from '../../../../../../shared/components/button/button.component';
+import { Store } from '@ngrx/store';
+import {
+  openDeleteMenuItemModal,
+  openUpdateMenuItemModal,
+} from '../../../../../../core/state/modal/menuItem/modal.actions';
 
 @Component({
-    selector: '[menuItem-auctions-table-item]',
-    templateUrl: './menuItem-auctions-table-item.component.html',
-    standalone: true,
-    imports: [AngularSvgIconModule, CurrencyPipe,ButtonComponent],
+  selector: '[menuItem-auctions-table-item]',
+  templateUrl: './menuItem-auctions-table-item.component.html',
+  standalone: true,
+  imports: [AngularSvgIconModule, CurrencyPipe, ButtonComponent],
 })
 export class MenuItemAuctionsTableItemComponent implements OnInit {
-  @Input() item: MenuItem = <MenuItem>{};
-  constructor() {}
+  @Input() menuItem: MenuItem = <MenuItem>{};
+  constructor(private store: Store) {}
 
+  ngOnInit(): void {
+    console.log();
+  }
 
-  ngOnInit(): void {  console.log()}
+  openUpdateModal() {
+    this.store.dispatch(openUpdateMenuItemModal({ menuItem: this.menuItem }));
+  }
+
+  openDeleteModal() {
+    this.store.dispatch(openDeleteMenuItemModal({ menuItemId: this.menuItem.$id }));
+  }
 }
