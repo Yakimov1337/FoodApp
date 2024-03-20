e302b51cc7be4051a45395223d4f4b7c99d6df76
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-21 00:40:04 +0200

    impl: auth service

--- src/app/modules/auth/pages/sign-up/sign-up.component.ts
+++ src/app/modules/auth/pages/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControlOptions } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 import { MustMatch } from './must-match.validator';
@@ -7,13 +7,13 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from 'src/app/shared/components/button/button.component';
-
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
+  // Ensure you include all necessary imports if using standalone components
   standalone: true,
-  imports: [FormsModule, RouterLink, AngularSvgIconModule, ButtonComponent, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, RouterLink, AngularSvgIconModule, ButtonComponent],
 })
 export class SignUpComponent implements OnInit {
   signUpForm!: FormGroup;
@@ -21,16 +21,15 @@ export class SignUpComponent implements OnInit {
   constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
-    this.signUpForm = this.fb.group(
-      {
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(3)]],
-        confirmPassword: ['', Validators.required],
-      },
-      {
-        validator: MustMatch('password', 'confirmPassword'),
-      },
-    );
+    const formOptions: AbstractControlOptions = {
+      validators: MustMatch('password', 'confirmPassword')
+    };
+
+    this.signUpForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', Validators.required],
+    }, formOptions);
   }
 
   get form() {
@@ -41,27 +40,23 @@ export class SignUpComponent implements OnInit {
     this.signUpForm.markAllAsTouched();
     if (this.signUpForm.valid) {
       const { email, password } = this.signUpForm.value;
-      this.authService
-        .createUserAccount({ email, password })
-        .then(() => {
-          this.authService.signInAccount(email, password);
-        })
-        .then(() => {
-          this.router.navigate(['/']);
-        })
-        .catch((error) => {
+      this.authService.createUserAccount({ email, password }).subscribe({
+        next: () => {
+          this.authService.signInAccount(email, password).subscribe({
+            next: () => {
+              this.router.navigate(['/']);
+            },
+            error: (error) => {
+              console.error('SignIn Error:', error);
+            },
+          });
+        },
+        error: (error) => {
           console.error('SignUp Error:', error);
-        });
+        },
+      });
     } else {
       console.error('Form is invalid');
-      Object.keys(this.signUpForm.controls).forEach((key) => {
-        const controlErrors = this.signUpForm.get(key)?.errors;
-        if (controlErrors != null) {
-          Object.keys(controlErrors).forEach((keyError) => {
-            console.log('Key control: ' + key + ', keyError: ' + keyError + ', err value: ', controlErrors[keyError]);
-          });
-        }
-      });
     }
   }
 }
