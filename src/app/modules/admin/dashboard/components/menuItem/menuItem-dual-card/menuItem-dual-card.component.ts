46156d4eccbc81783ed304e1f251c974ba5336c0
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-20 23:42:09 +0200

    fix: dual card fix

--- src/app/modules/admin/dashboard/components/menuItem/menuItem-dual-card/menuItem-dual-card.component.ts
+++ src/app/modules/admin/dashboard/components/menuItem/menuItem-dual-card/menuItem-dual-card.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MenuItem } from '../../../models/food-menu-item';
 import { NgStyle, CurrencyPipe } from '@angular/common';
+import { MenuItem } from '../../../../../../core/models';
 
 @Component({
-    selector: '[menuItem-dual-card]',
-    templateUrl: './menuItem-dual-card.component.html',
-    standalone: true,
-    imports: [NgStyle, CurrencyPipe],
+  selector: '[menuItem-dual-card]',
+  templateUrl: './menuItem-dual-card.component.html',
+  standalone: true,
+  imports: [NgStyle, CurrencyPipe],
 })
-export class MenuItemDualCardComponent implements OnInit {
+export class MenuItemDualCardComponent  {
   @Input() menuItem: MenuItem = <MenuItem>{};
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit(): void {}
 }
