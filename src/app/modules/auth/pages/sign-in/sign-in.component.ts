847d552edaac97696905745217f155181c7ef9f9
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-23 04:43:56 +0200

    fix: validators and error logs

--- src/app/modules/auth/pages/sign-in/sign-in.component.ts
+++ src/app/modules/auth/pages/sign-in/sign-in.component.ts
@@ -4,12 +4,15 @@ import { Router, RouterLink } from '@angular/router';
 import { NgClass, NgIf } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
-import { account } from '../../../../core/lib/appwrite';
+import { AuthService } from '../../../../services/auth.service';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../../../../core/state/auth/auth.actions';
+import { ToastrService } from 'ngx-toastr';
+import { environment } from '../../../../../environments/environment.local';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss'],
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule, RouterLink, AngularSvgIconModule, NgClass, NgIf, ButtonComponent],
 })
@@ -17,12 +20,63 @@ export class SignInComponent implements OnInit {
   form!: FormGroup;
   submitted = false;
   passwordTextType!: boolean;
-  constructor(private formBuilder: FormBuilder, private router: Router) {}
+  isLoadingUser = false;
+  isLoadingAdmin = false;
+  isLoadingModerator = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authService: AuthService,
+    private store: Store,
+    private toastr: ToastrService,
+  ) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  // Admin Login
+  loginAsAdmin() {
+    this.isLoadingAdmin = true;
+    const { email, password } = environment.adminCredentials;
+    this.login(email, password, 'admin');
+  }
+
+  // Moderator Login
+  loginAsModerator() {
+    this.isLoadingModerator = true;
+    const { email, password } = environment.moderatorCredentials;
+    this.login(email, password, 'moderator');
+  }
+
+  // Normal User Login
+  loginAsUser() {
+    if (this.form.valid) {
+      this.isLoadingUser = true;
+      const { email, password } = this.form.value;
+      this.login(email, password, 'user');
+    } else {
+      this.toastr.error('Please fill the form with proper values.');
+    }
+  }
+
+  // Common login method
+  private login(email: string, password: string, type: 'user' | 'admin' | 'moderator') {
+    this.authService.signInAccount(email, password).subscribe({
+      next: () => {
+        // User is successfully authenticated, dispatch the login action
+        this.store.dispatch(AuthActions.login());
+        (this as any)[`isLoading${type.charAt(0).toUpperCase() + type.slice(1)}`] = false; // Converts to camelCase
+      },
+      error: (error) => {
+        console.error('Error signing in:', error);
+        this.toastr.error('Login failed.', error);
+        (this as any)[`isLoading${type.charAt(0).toUpperCase() + type.slice(1)}`] = false;
+      },
     });
   }
 
@@ -32,27 +86,5 @@ export class SignInComponent implements OnInit {
 
   togglePasswordTextType() {
     this.passwordTextType = !this.passwordTextType;
-  }
-
-  async onSubmit() {
-    this.submitted = true;
-
-    // Stop here if the form is invalid
-    if (this.form.invalid) {
-      return;
-    }
-
-    const { email, password } = this.form.value;
-
-    try {
-      // Use the existing Appwrite account instance to create an email session
-      await account.createEmailSession(email, password);
-
-      // If successful, navigate to the dashboard or home page
-      this.router.navigate(['/']);
-    } catch (error) {
-      console.error('Error logging in:', error);
-      // Handle login error, e.g., show an error message to the user
-    }
   }
 }
