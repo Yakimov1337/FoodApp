5d3e198bbe2b3ae4ea38ac335782a64db26145d9
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-23 04:43:52 +0200

    fix: validators and error logs

--- src/app/modules/auth/pages/sign-up/sign-up.component.ts
+++ src/app/modules/auth/pages/sign-up/sign-up.component.ts
@@ -7,29 +7,47 @@ import { AngularSvgIconModule } from 'angular-svg-icon';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from 'src/app/shared/components/button/button.component';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
+import { Store } from '@ngrx/store';
+import * as AuthActions from 'src/app/core/state/auth/auth.actions'
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss'],
-  // Ensure you include all necessary imports if using standalone components
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, RouterLink, AngularSvgIconModule, ButtonComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink, AngularSvgIconModule, ButtonComponent],
 })
 export class SignUpComponent implements OnInit {
   signUpForm!: FormGroup;
+  isLoading = false;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {}
+  //Progressive password bar
+  get passwordStrength(): boolean[] {
+    const length = this.signUpForm.get('password')?.value.length || 0;
+    return [1, 2, 3, 4].map((i) => length >= i * 2);
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private toastr: ToastrService,
+    private store: Store,
+  ) {}
 
   ngOnInit() {
     const formOptions: AbstractControlOptions = {
-      validators: MustMatch('password', 'confirmPassword')
+      validators: MustMatch('password', 'confirmPassword'),
     };
 
-    this.signUpForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', Validators.required],
-    }, formOptions);
+    this.signUpForm = this.fb.group(
+      {
+        email: ['', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', Validators.required],
+      },
+      formOptions,
+    );
   }
 
   get form() {
@@ -38,25 +56,30 @@ export class SignUpComponent implements OnInit {
 
   onSubmit() {
     this.signUpForm.markAllAsTouched();
+    this.isLoading = true;
     if (this.signUpForm.valid) {
       const { email, password } = this.signUpForm.value;
       this.authService.createUserAccount({ email, password }).subscribe({
-        next: () => {
+        next: (userCreatedResponse) => {
           this.authService.signInAccount(email, password).subscribe({
-            next: () => {
-              this.router.navigate(['/']);
+            next: (userSignInResponse) => {
+              this.store.dispatch(AuthActions.login());
+              this.isLoading = false;
             },
             error: (error) => {
-              console.error('SignIn Error:', error);
+              this.toastr.error(`Sign In Error: ${error}`);
+              this.isLoading = false;
             },
           });
         },
         error: (error) => {
-          console.error('SignUp Error:', error);
+          this.toastr.error(`SignUp Error: ${error}`);
+          this.isLoading = false;
         },
       });
     } else {
-      console.error('Form is invalid');
+      this.toastr.error('Form is invalid');
+      this.isLoading = false;
     }
   }
 }
