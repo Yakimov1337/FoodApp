30ce7f8d03237ae41187fa4beaf5feb90abc4f62
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-27 11:07:41 +0200

    impl: user home page

--- src/app/modules/user/pages/home/home.component.ts
+++ src/app/modules/user/pages/home/home.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../../services/auth.service';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../../../../core/state/auth/auth.actions';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { FoodCategoryComponent } from './components/food-category/food-category.component';
+import { InfoSectionComponent } from './components/info-section/info-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,HeroSectionComponent,InfoSectionComponent,FoodCategoryComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
-
+  constructor(private router: Router, private authService: AuthService, private store: Store) {}
+  logout() {
+    this.store.dispatch(AuthActions.logout());
+  }
 }
