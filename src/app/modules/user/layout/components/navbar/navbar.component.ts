c90895ad23858921e5e1a398b9f0955f1521702a
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-27 11:04:02 +0200

    impl: user navbar

---
+++ src/app/modules/user/layout/components/navbar/navbar.component.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet, Event, RouterLink } from '@angular/router';
+import { ProfileMenuComponent } from './profile-menu/profile-menu.component';
+import { Observable } from 'rxjs';
+import { User } from '../../../../../core/models';
+import { Store, select } from '@ngrx/store';
+import { selectCurrentUser } from '../../../../../core/state/auth/auth.selectors';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, ProfileMenuComponent, RouterLink],
+  templateUrl: './navbar.component.html',
+})
+export class NavbarComponent {
+  private mainContent: HTMLElement | null = null;
+  isScrolled = false;
+  mobileMenuOpen = false;
+  user$: Observable<User | null>;
+
+  constructor(private router: Router, private store: Store) {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        if (this.mainContent) {
+          this.mainContent!.scrollTop = 0;
+        }
+      }
+    });
+    this.user$ = this.store.pipe(select(selectCurrentUser));
+  }
+
+  ngOnInit(): void {
+    this.mainContent = document.getElementById('main-content');
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 0;
+    console.log(this.isScrolled);
+  }
+
+  toggleMobileMenu(): void {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+}
