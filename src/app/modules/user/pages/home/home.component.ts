9e5a2b23c95a3a7a47a7b4c80395f06690c3a766
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-29 03:54:13 +0200

    impl: home component

--- src/app/modules/user/pages/home/home.component.ts
+++ src/app/modules/user/pages/home/home.component.ts
@@ -6,16 +6,43 @@ import * as AuthActions from '../../../../core/state/auth/auth.actions';
 import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { FoodCategoryComponent } from './components/food-category/food-category.component';
 import { InfoSectionComponent } from './components/info-section/info-section.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ThemeService } from '../../../../services/theme.service';
+import { CommonModule } from '@angular/common';
+import { SubscribeComponent } from './components/subscribe/subscribe.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink,HeroSectionComponent,InfoSectionComponent,FoodCategoryComponent],
+  imports: [
+    RouterLink,
+    HeroSectionComponent,
+    InfoSectionComponent,
+    FoodCategoryComponent,
+    PricingComponent,
+    SubscribeComponent,
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  constructor(private router: Router, private authService: AuthService, private store: Store) {}
+  isDarkMode: boolean;
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private store: Store,
+    public themeService: ThemeService,
+  ) {
+    this.isDarkMode = this.themeService.isDark;
+  }
   logout() {
     this.store.dispatch(AuthActions.logout());
+  }
+
+  checkThemeMode() {
+    this.themeService.theme.update((theme) => {
+      const mode = !this.themeService.isDark ? 'dark' : 'light';
+      return { ...theme, mode: mode };
+    });
   }
 }
