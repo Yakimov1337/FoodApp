774b6b7e4a7f0a64590b701ebbf6ee47505c1b47
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-29 03:55:22 +0200

    fix: categories added

--- src/app/modules/user/pages/home/components/food-category/food-category.component.ts
+++ src/app/modules/user/pages/home/components/food-category/food-category.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-food-category',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './food-category.component.html',
 })
 export class FoodCategoryComponent {
+  constructor(private router: Router) {}
+  navigateToMenuWithCategory(category: string): void {
+    this.router.navigate(['/menu'], { queryParams: { category: category.toLowerCase() } });
+  }
 
   foods = [
     {
@@ -15,50 +20,56 @@ export class FoodCategoryComponent {
       description: 'Ground turkey, cheese, hamburger buns, black pepper',
       price: '20$',
       imageUrl: 'assets/images/burger-home.jpg',
+      category: 'Burger',
     },
     {
       name: 'Pizza',
       description: 'Ground turkey, cheese, hamburger buns, black pepper',
       price: '20$',
       imageUrl: 'assets/images/pizza.jpg',
+      category: 'Pizza',
     },
     {
       name: 'Pasta',
       description: 'Ground turkey, cheese, hamburger buns, black pepper',
       price: '20$',
       imageUrl: 'assets/images/pasta.jpg',
+      category: 'Pasta',
+    },
+    {
+      name: 'Sushi',
+      description: 'Ground turkey, cheese, hamburger buns, black pepper',
+      price: '20$',
+      imageUrl: 'assets/images/other.jpg',
+      category: 'Sushi',
+    },
+    {
+      name: 'Salad',
+      description: 'Ground turkey, cheese, hamburger buns, black pepper',
+      price: '20$',
+      imageUrl: 'assets/images/burger-home.jpg',
+      category: 'Salad',
+    },
+    {
+      name: 'Tacos',
+      description: 'Ground turkey, cheese, hamburger buns, black pepper',
+      price: '20$',
+      imageUrl: 'assets/images/burger-home.jpg',
+      category: 'Tacos',
+    },
+    {
+      name: 'Dessert',
+      description: 'Ground turkey, cheese, hamburger buns, black pepper',
+      price: '20$',
+      imageUrl: 'assets/images/burger-home.jpg',
+      category: 'Dessert',
     },
     {
       name: 'Other',
       description: 'Ground turkey, cheese, hamburger buns, black pepper',
       price: '20$',
-      imageUrl: 'assets/images/other.jpg',
-    },
-    {
-      name: 'Burgers',
-      description: 'Ground turkey, cheese, hamburger buns, black pepper',
-      price: '20$',
       imageUrl: 'assets/images/burger-home.jpg',
-    },
-    {
-      name: 'Burgers',
-      description: 'Ground turkey, cheese, hamburger buns, black pepper',
-      price: '20$',
-      imageUrl: 'assets/images/burger-home.jpg',
-    },
-    {
-      name: 'Burgers',
-      description: 'Ground turkey, cheese, hamburger buns, black pepper',
-      price: '20$',
-      imageUrl: 'assets/images/burger-home.jpg',
-    },
-    {
-      name: 'Burgers',
-      description: 'Ground turkey, cheese, hamburger buns, black pepper',
-      price: '20$',
-      imageUrl: 'assets/images/burger-home.jpg',
+      category: 'Other',
     },
   ];
-
-
 }
