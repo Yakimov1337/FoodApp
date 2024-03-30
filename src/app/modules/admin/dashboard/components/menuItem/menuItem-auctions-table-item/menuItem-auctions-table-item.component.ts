6432dd466cee75fd267827685b8e46f3a7db8d04
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-30 23:13:25 +0200

    impl: role based permissions

--- src/app/modules/admin/dashboard/components/menuItem/menuItem-auctions-table-item/menuItem-auctions-table-item.component.ts
+++ src/app/modules/admin/dashboard/components/menuItem/menuItem-auctions-table-item/menuItem-auctions-table-item.component.ts
@@ -1,26 +1,39 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CurrencyPipe } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { AngularSvgIconModule } from 'angular-svg-icon';
-import { MenuItem } from '../../../../../../core/models';
+import { MenuItem, User } from '../../../../../../core/models';
 import { ButtonComponent } from '../../../../../../shared/components/button/button.component';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import {
   openDeleteMenuItemModal,
   openUpdateMenuItemModal,
 } from '../../../../../../core/state/modal/menuItem/modal.actions';
+import { selectCurrentUser } from '../../../../../../core/state/auth/auth.selectors';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: '[menuItem-auctions-table-item]',
   templateUrl: './menuItem-auctions-table-item.component.html',
   standalone: true,
-  imports: [AngularSvgIconModule, CurrencyPipe, ButtonComponent],
+  imports: [AngularSvgIconModule, CurrencyPipe, ButtonComponent, CommonModule],
 })
 export class MenuItemAuctionsTableItemComponent implements OnInit {
   @Input() menuItem: MenuItem = <MenuItem>{};
-  constructor(private store: Store) {}
+  currentUser$: Observable<User | null>;
+
+  constructor(private store: Store) {
+    this.currentUser$ = this.store.pipe(select(selectCurrentUser));
+  }
 
   ngOnInit(): void {
     console.log();
+  }
+
+  handleUpdateButtonClick(currentUser: User, menuItem: MenuItem): void {
+    // Only Admin can update default items
+    if (currentUser.role === 'Admin' || currentUser.role === 'Moderator' && !menuItem.default) {
+      this.openUpdateModal();
+    }
   }
 
   openUpdateModal() {
