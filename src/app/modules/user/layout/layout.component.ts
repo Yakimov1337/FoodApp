328437c66e2d7d11b1d215cdf954c7374111fe4c
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-03-31 18:51:57 +0300

    impl: shopping cart

--- src/app/modules/user/layout/layout.component.ts
+++ src/app/modules/user/layout/layout.component.ts
@@ -5,12 +5,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroSectionComponent } from '../pages/home/components/hero-section/hero-section.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CartComponent } from './components/cart/cart.component';
 
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   standalone: true,
-  imports: [RouterOutlet, CommonModule , NavbarComponent,FooterComponent],
+  imports: [RouterOutlet, CommonModule , NavbarComponent,FooterComponent,CartComponent],
 })
 export class LayoutComponent {}
