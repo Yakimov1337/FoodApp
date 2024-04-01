0e45a479345875d6a70e0e7995f86c2a504954bc
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-04-01 21:36:02 +0300

    impl: payment routes added

--- src/app/modules/user/layout/layout-routing.module.ts
+++ src/app/modules/user/layout/layout-routing.module.ts
@@ -4,6 +4,11 @@ import { LayoutComponent } from './layout.component';
 import { HomeComponent } from '../pages/home/home.component';
 import { ContactUsComponent } from '../pages/contact-us/contact-us.component';
 import { MenuComponent } from '../pages/menu/menu.component';
+import { UserProfileComponent } from '../pages/user-profile/user-profile.component';
+import { AboutUsComponent } from '../pages/about-us/about-us.component';
+import { OrdersComponent } from '../pages/orders/orders.component';
+import { SuccessComponent } from '../pages/stripe/success/success.component';
+import { CancelledComponent } from '../pages/stripe/cancelled/cancelled.component';
 
 const routes: Routes = [
   {
@@ -12,7 +17,12 @@ const routes: Routes = [
     children: [
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'menu', component: MenuComponent },
+      { path: 'profile', component: UserProfileComponent },
+      { path: 'orders', component: OrdersComponent },
+      { path: 'about-us', component: AboutUsComponent },
       { path: 'contact-us', component: ContactUsComponent },
+      { path: 'payment-success', component: SuccessComponent },
+      { path: 'payment-cancel', component: CancelledComponent },
     ],
   },
 ];
